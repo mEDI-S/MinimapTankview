49dcaece0d4130038a578081260a5dea8114a6b6
mEDI <medi@255.li>
2013-11-19 03:48:13 +0100

    convert space to tab

--- src/uragan_minimap_tankview.py
+++ src/uragan_minimap_tankview.py
@@ -1,4 +1,4 @@
-# Embedded file name: scripts/client/gui/Scaleform/released\uragan_minimap_tankview.py
+﻿# Embedded file name: scripts/client/gui/Scaleform/released\uragan_minimap_tankview.py
 """
 UrAGAn 
 Script uragan_minimap_tankview.py
@@ -14,53 +14,53 @@ import BattleReplay
 enemymodellist = {}
 
 def minimapvhc():
-    player = BigWorld.player()
-    if player is None:
-        return
-    else:
-        replayCtrl = BattleReplay.g_replayCtrl
-        if replayCtrl.isPlaying:
-            return
-        if not hasattr(player, 'isOnArena'):
-            return
-        if player.isOnArena:
-            arena = player.arena
-            vehicles = arena.vehicles
-            for vehicleID, desc in vehicles.items():
-                activeTurretPosition = desc['vehicleType'].activeTurretPosition
-                if activeTurretPosition != 0:
-                    LOG_WARNING(activeTurretPosition)
-                if player.team is not vehicles[vehicleID]['team'] or 'SPG' in desc['vehicleType'].type.tags:
-                    if desc['isAlive'] and player.playerVehicleID != vehicleID:
-                        entity = BigWorld.entity(vehicleID)
-                        if entity is not None:
-                            m, rot = getEntryMatrix(vehicleID)
-                            mm = Math.WGCombinedMP()
-                            mm.translationSrc = m.source
-                            mm.rotationSrc = rot
-                            enemymodellist[vehicleID] = g_windowsManager.battleWindow.minimap._Minimap__ownUI.addEntry(mm, g_windowsManager.battleWindow.minimap.zIndexManager.getVehicleIndex(vehicleID) - 100)
-                            g_windowsManager.battleWindow.minimap._Minimap__ownUI.entryInvoke(enemymodellist[vehicleID], ('gotoAndStop', ['cursorNormal']))
-                        elif enemymodellist.has_key(vehicleID):
-                            g_windowsManager.battleWindow.minimap._Minimap__ownUI.delEntry(enemymodellist[vehicleID])
-                            enemymodellist.pop(vehicleID)
-                    elif enemymodellist.has_key(vehicleID):
-                        g_windowsManager.battleWindow.minimap._Minimap__ownUI.delEntry(enemymodellist[vehicleID])
-                        enemymodellist.pop(vehicleID)
+	player = BigWorld.player()
+	if player is None:
+		return
+	else:
+		replayCtrl = BattleReplay.g_replayCtrl
+		if replayCtrl.isPlaying:
+			return
+		if not hasattr(player, 'isOnArena'):
+			return
+		if player.isOnArena:
+			arena = player.arena
+			vehicles = arena.vehicles
+			for vehicleID, desc in vehicles.items():
+				activeTurretPosition = desc['vehicleType'].activeTurretPosition
+				if activeTurretPosition != 0:
+					LOG_WARNING(activeTurretPosition)
+				if player.team is not vehicles[vehicleID]['team'] or 'SPG' in desc['vehicleType'].type.tags:
+					if desc['isAlive'] and player.playerVehicleID != vehicleID:
+						entity = BigWorld.entity(vehicleID)
+						if entity is not None:
+							m, rot = getEntryMatrix(vehicleID)
+							mm = Math.WGCombinedMP()
+							mm.translationSrc = m.source
+							mm.rotationSrc = rot
+							enemymodellist[vehicleID] = g_windowsManager.battleWindow.minimap._Minimap__ownUI.addEntry(mm, g_windowsManager.battleWindow.minimap.zIndexManager.getVehicleIndex(vehicleID) - 100)
+							g_windowsManager.battleWindow.minimap._Minimap__ownUI.entryInvoke(enemymodellist[vehicleID], ('gotoAndStop', ['cursorNormal']))
+						elif enemymodellist.has_key(vehicleID):
+							g_windowsManager.battleWindow.minimap._Minimap__ownUI.delEntry(enemymodellist[vehicleID])
+							enemymodellist.pop(vehicleID)
+					elif enemymodellist.has_key(vehicleID):
+						g_windowsManager.battleWindow.minimap._Minimap__ownUI.delEntry(enemymodellist[vehicleID])
+						enemymodellist.pop(vehicleID)
 
-        return
+		return
 
 
 def getEntryMatrix(id):
-    matrix = BigWorld.entities[id].matrix
-    rotMatrix = BigWorld.entities[id].appearance.modelsDesc['gun']['model'].matrix
-    m = Math.WGTranslationOnlyMP()
-    m.source = matrix
-    return (m, rotMatrix)
+	matrix = BigWorld.entities[id].matrix
+	rotMatrix = BigWorld.entities[id].appearance.modelsDesc['gun']['model'].matrix
+	m = Math.WGTranslationOnlyMP()
+	m.source = matrix
+	return (m, rotMatrix)
 
 
 def minimapvhc_clb():
-    minimapvhc()
-    BigWorld.callback(0.1, minimapvhc_clb)
+	minimapvhc()
+	BigWorld.callback(0.1, minimapvhc_clb)
 
 
 minimapvhc_clb()
