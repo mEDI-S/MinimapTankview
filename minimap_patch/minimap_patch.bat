e9ea29bae57636e273b5767d043eace9f94b4f1e
mEDI <medi@255.li>
2014-02-18 18:45:51 +0100

    add minimap_line.patch other pointer version

--- minimap_patch/minimap_patch.bat
+++ minimap_patch/minimap_patch.bat
@@ -1,5 +1,9 @@
 @echo off
 
 swfmill swf2xml Minimap.swf Minimap.swf.xml
-patch --binary -p0 < minimap.patch
-swfmill xml2swf Minimap.swf.xml Minimap_new.swf
+patch --binary -p0 < minimap_arrow.patch
+swfmill xml2swf Minimap.swf.xml Minimap_arrow.swf
+
+swfmill swf2xml Minimap.swf Minimap.swf.xml
+patch --binary -p0 < Minimap_line.patch
+swfmill xml2swf Minimap.swf.xml Minimap_line.swf
